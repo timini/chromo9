41179678ad97e62738e5413cffb5e50db0d4e164
George Seed <gseed01@fortuna.cryst.bbk.ac.uk>
2014-03-18 19:51:05 +0000

    CGI + html page frameworks

---
+++ cgi-bin/webcall.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+use CGI;
+use strict;
+use warnings;
+my $cgi = new CGI;
+my $val = $cgi->param('search');
+
+print $cgi->header();
+print <<EOF;
+<html>
+<head>
+<title>Hello there.</title>
+</head>
+<body>
+<p>Your name is</p>
+<p>$val</>
+</body>
+</html>
+EOF
