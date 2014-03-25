1effae90ba2aa27092f07e3742b3f2208dcb78de
George Seed <gseed01@fortuna.cryst.bbk.ac.uk>
2014-03-25 19:08:53 +0000

    'Search' now posting two parameters - name and type of search. Index page updated and cgi updated appropriately. HTML formatting improved but still needs work. 'Details' kind of broken, need to update cgi.

--- cgi-bin/webcall.pl
+++ cgi-bin/webcall.pl
@@ -2,50 +2,53 @@
 use CGI;
 use strict;
 use warnings;
-my $cgi = new CGI;
 
+my $cgi = new CGI;
 my $cmd = $cgi->url_param('cmd');
 
 if ($cmd eq "search") {
 Search();
-} elsif ($cmd eq "gene") {
-Gene();
+} elsif ($cmd eq "detail") {
+GeneDetails();
 }
 
 
 sub Search
 {
-my $searchString = $cgi->param('search');
-
+my $searchString = $cgi->param('query');
+my $searchOption = $cgi->param('type');
 print $cgi->header();
 print <<EOF;
 <html>
 <head>
-<link rel='stylesheet' type='text/css' href='../home.css'/>
+<link rel='stylesheet' type='text/css' href='http://student.cryst.bbk.ac.uk/~gseed01/web/home.css'/>
 </head>
 <body>
-<div id="container">This contains most everything.. I think</div>
+<div id="container"></div>
 <div id="Header">This is the header/title area - put an image maybe?</div>
 <div id="LeftTab">This is the left toolbar/set of links</div>
 <p>The Command is: $cmd</p>
 <p>$searchString</p>
+<p>searchoption = $searchOption</p>
 </body>
 </html>
 EOF
-
 }
 
-sub Gene
+sub GeneDetails
 {
-my $geneName = $cgi->url_param('geneName');
+my $geneName = $cgi->url_param('details');
 
 print $cgi->header();
 print <<EOF;
 <html>
 <head>
-<link rel='stylesheet' type='text/css' href='../home.css'/>
+<link rel='stylesheet' type='text/css' href='http://student.cryst.bbk.ac.uk/~gseed01/web/home.css'/>
 </head>
 <body>
+<div id="container"></div>
+<div id="Header">This is the header/title area - put an image maybe?</div>
+<div id="LeftTab">This is the left toolbar/set of links</div>
 <p>The Command is: $cmd</p>
 <p>$geneName</p>
 </body>
